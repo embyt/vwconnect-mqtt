576e6b1ff76e701a4b03b38df3a969e3a5ef0ded
Roman Morawek <roman.morawek@embyt.com>
2021-12-15 20:42:43 +0100

    use config file for settings

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 import * as api from "./vwconnectapi";
+import nconf from "nconf";
 
-// check command line args
-if (process.argv.length !== 4) {
-  console.error(`Usage: ${process.argv[1]} <username> <password>`);
-  process.exit();
-}
-const username = process.argv[2];
-const password = process.argv[3];
+// setup configuration
+nconf
+  .argv()
+  .env()
+  .file({ file: `${__dirname}/vwconnect-mqtt.conf` })
+  .defaults({
+    vwc_type: "id",
+  });
 
 var vwConn = new api.VwWeConnect();
 vwConn.setLogLevel("INFO"); // optional, ERROR (default), INFO, WARN or DEBUG
-vwConn.setCredentials(username, password, "");
-vwConn.setConfig("id"); // type
+vwConn.setCredentials(nconf.get("vwc_username"), nconf.get("vwc_password"), undefined);
+vwConn.setConfig(nconf.get("vwc_type")); // type
 vwConn.getData().then(() => {
-  console.log("SOC", vwConn.idData.data.batteryStatus.currentSOC_pct + "%");
-  console.log("batteryStatus", vwConn.idData.data.batteryStatus);
   console.log("data", vwConn.idData.data);
-  console.log("iddata", vwConn.idData);
-  console.log("con", vwConn);
+  console.log("vinArray", vwConn.vinArray);
 });
