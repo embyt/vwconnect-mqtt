e934d77f357b52969ec01c587ec3e43b94a6b084
Roman Morawek <roman.morawek@embyt.com>
2021-12-16 09:08:57 +0100

    allow subscribing to demanded data topics

--- src/index.ts
+++ src/index.ts
@@ -4,61 +4,103 @@ import { dirname } from "path";
 import mqtt from "mqtt";
 import * as api from "./vwconnectapi.cjs";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+function setupConfig() {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  // setup configuration
+  nconf
+    .argv()
+    .env()
+    .file({ file: `${__dirname}/vwconnect-mqtt.conf.json` })
+    .defaults({
+      vwc: {
+        type: "id",
+        pollInterval: 10 * 60, // s
+      },
+      mqtt: {
+        prefix: "vwconnect",
+      },
+      data: {
+        timestamp: "batteryStatus.carCapturedTimestamp",
+        soc: "batteryStatus.currentSOC_pct",
+        range: "batteryStatus.cruisingRangeElectric_km",
+        charging_power: "chargingStatus.chargePower_kW",
+        remaining_charging_time: "chargingStatus.remainingChargingTimeToComplete_min",
+        target_soc: "chargingSettings.targetSOC_pct",
+      },
+    });
+}
 
-// setup configuration
-nconf
-  .argv()
-  .env()
-  .file({ file: `${__dirname}/vwconnect-mqtt.conf.json` })
-  .defaults({
-    vwc: {
-      type: "id",
-      pollInterval: 10 * 60, // s
-    },
-    mqtt: {
-      prefix: "vwconnect",
-    },
+function setupVwConnectApi() {
+  // setup vw connect interface
+  const vwConn = new api.VwWeConnect();
+  vwConn.setLogLevel("INFO"); // optional, ERROR (default), INFO, WARN or DEBUG
+  vwConn.setCredentials(nconf.get("vwc:username"), nconf.get("vwc:password"), undefined);
+  vwConn.setConfig(nconf.get("vwc:type")); // type
+  return vwConn;
+}
+
+function setupMqtt() {
+  // setup mqtt interface
+  const mqttclient = mqtt.connect(nconf.get("mqtt:host"), {
+    clientId: "vwconnect",
+    username: nconf.get("mqtt:user"),
+    password: nconf.get("mqtt:password"),
   });
+  mqttclient.on("connect", () => {
+    console.log("mqtt: connected");
+  });
+  mqttclient.on("error", (error) => {
+    console.error("mqtt: Can't connect", error);
+  });
+  return mqttclient;
+}
 
-// setup vw connect interface
-var vwConn = new api.VwWeConnect();
-vwConn.setLogLevel("INFO"); // optional, ERROR (default), INFO, WARN or DEBUG
-vwConn.setCredentials(nconf.get("vwc:username"), nconf.get("vwc:password"), undefined);
-vwConn.setConfig(nconf.get("vwc:type")); // type
+function extractData(data: api.IIdData) {
+  // loop through all subscribed data topics
+  const topics = Object.keys(nconf.get("data"));
+  const values = topics.map((curTopic) => {
+    let curObj: any = data;
+    const topicParts = curTopic.split(".");
+    topicParts.forEach((curSect) => {
+      curObj = curObj?.[curSect];
+    });
+    return curObj;
+  });
+  console.log("data", topics, values);
 
-// setup mqtt interface
-const mqttclient = mqtt.connect(nconf.get("mqtt:host"), {
-  clientId: "vwconnect",
-  username: nconf.get("mqtt:user"),
-  password: nconf.get("mqtt:password"),
-});
-mqttclient.on("connect", () => {
-  console.log("mqtt: connected");
-});
-mqttclient.on("error", (error) => {
-  console.error("mqtt: Can't connect", error);
-});
+  // publish to mqtt
+  for (let i = 0; i < topics.length; i++) {
+    if (values[i] !== undefined) {
+      mqttclient.publish(`${nconf.get("mqtt:prefix")}/${topics[i]}`, `${values[i]}`);
+    } else {
+      console.warn("topic not found:", topics[i]);
+    }
+  }
+}
+
+setupConfig();
+const vwConn = setupVwConnectApi();
+const mqttclient = setupMqtt();
 
 // login, read vehicles and initial read of data
 console.log("login and get initial status");
-let data = await vwConn.getData();
+await vwConn.getData();
 
 // start endless loop
 while (true) {
-  console.log("batteryStatus", vwConn.idData.data.batteryStatus);
-  mqttclient.publish(
-    `${nconf.get("mqtt:prefix")}/soc`,
-    vwConn.idData.data.batteryStatus.currentSOC_pct,
-    { retain: true, qos: 1 },
-  );
+  // publish data to mqtt
+  extractData(vwConn.idData);
 
+  // renew communication tokens
   console.log("refresh token");
   await vwConn.refreshIDToken().catch(() => {
     console.error("error refreshing id token");
   });
 
+  // pause
   await new Promise((resolve) => setTimeout(resolve, nconf.get("vwc_pollInterval") * 1000));
+
+  // get data
   console.log("get status");
   await Promise.all(
     vwConn.vinArray.map((vin) => {
